b3833efdd5b099128c293d718f4bf6c599d56042
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-08-08 23:53:31 +0530

    dsa

---
+++ assesmodifiers/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("I am in main class.");
+        main2 m2 = new main2();
+        m2.printMessage();
+        // m2.alpha();
+    }
+}
---
+++ assesmodifiers/main2.java
@@ -0,0 +1,9 @@
+public class main2 {
+    public void  printMessage() {
+        System.out.println("I am in main2 class.");
+        alpha();
+    }
+    public static void alpha(){
+        System.out.println("This is static function");
+    }
+}
