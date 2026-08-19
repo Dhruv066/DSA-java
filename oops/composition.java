40a9c2ddb5ebae9da6f11fffb0e2f5c1e75e885e
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-08-19 23:38:46 +0530

    composition

---
+++ oops/composition.java
@@ -0,0 +1,18 @@
+package oops;
+
+class Engine{
+    void run(){
+        System.out.println("engine is running.. ");
+
+    }
+}
+class Truck{
+    private Engine engine ;
+    Truck(){
+        engine = new Engine();
+    }
+    void start(){
+        System.out.println("truck is starting. ");
+    }
+
+}
