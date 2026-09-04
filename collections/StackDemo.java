02ef71319243381405b7ecae47f842f156675102
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-09-04 22:19:24 +0530

    Stack data

---
+++ collections/StackDemo.java
@@ -0,0 +1,14 @@
+package collections;
+import java.util.Stack;
+
+public class StackDemo {
+    static void main() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
+}
