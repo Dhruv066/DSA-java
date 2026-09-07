cf90ae1e225c1606a7a320bb61d8f9c3b4f62b5a
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-09-07 21:52:32 +0530

    add didgit at front

--- maths/DigitManupulation.java
+++ maths/DigitManupulation.java
@@ -10,12 +10,32 @@ public class DigitManupulation {
             return num*10+digit;
         }
     }
+    static int addDigitsAtfront(int num,int digit) {
+        int count = 0;
+        boolean isNegative=false;
+
+
+        if (num<0) {
+            num=-1*num;
+            isNegative=true;
+
+        }
+        count=(int)Math.log10(num)+1;
+
+        int res = digit *(int) Math.pow(10,count)+num;
+        if (isNegative){
+            return -1*res;
+        }
+        return res;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number :");
         int num = sc.nextInt();
         System.out.println("Enter the digit which you want to add :");
         int digit = sc.nextInt();
-        System.out.println(addDigits(num,digit));
+//        System.out.println(addDigits(num,digit));
+        System.out.println(addDigitsAtfront(num,digit));
     }
+
 }
