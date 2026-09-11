5fe2fb5691a758f7924b5e2d5cda49e0e6cb8e58
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-09-11 22:40:04 +0530

    decimal to any base

--- maths/BaseConversion.java
+++ maths/BaseConversion.java
@@ -32,16 +32,46 @@ public class BaseConversion {
 
         System.out.print("Converted number is : " + res);
     }
-
+    private static char getChar(int r){
+        switch (r){
+            case 10:
+                return 'A';
+            case 11:
+                return 'B';
+            case 12:
+                return 'C';
+            case 13:
+                return 'D';
+            case 14:
+                return 'E';
+            case 15:
+                return 'F';
+            default:
+                return (char)(r+'0');
+        }
+    }
+static void convertFromDecimal(int  s, int n) {
+        StringBuilder res = new StringBuilder();
+    while(s>0){
+        int r = s % n;
+        char ch = getChar(r);
+        res.append(ch);
+        s = s/n;
+    }
+    System.out.println("Converted number is : " + res.reverse());
+}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        String a = sc.nextLine();
+//        System.out.print("Enter a number: ");
+//        String a = sc.nextLine();
+        System.out.println("Enter the number :");
+        int  n = sc.nextInt();
 
         System.out.print("Enter the base of the number: ");
         int b = sc.nextInt();
 
-        convertToDecimal(a, b);
+//        convertToDecimal(a, b);
+        convertFromDecimal(n,b);
     }
 }
