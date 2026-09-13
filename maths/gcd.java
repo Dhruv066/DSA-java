5c0ed32420e0bea7e7c00bd2999368da4f1c6e21
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-09-13 23:23:31 +0530

    norma gcd

---
+++ maths/gcd.java
@@ -0,0 +1,31 @@
+package maths;
+
+import java.util.Scanner;
+
+public class gcd {
+    private static int findGcd(int a, int b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+//        cases for handling zero:
+        if(a==0 && b==0) return 0;
+        if(a==0) return b;
+        if(b==0) return a;
+
+        int min = Math.min(a,b);
+        int res=1;
+        for(int i=2;i<=min;i++){
+            if(a%i==0 && b%i==0){
+                res = i;
+            }
+        }
+        return res;
+    }
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 number: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(findGcd(a, b));
+    }
+}
