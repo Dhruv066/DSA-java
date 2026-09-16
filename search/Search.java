50ee12198c881bb8b41e3f26ee87e081bcceed3d
Dhruv Maurya <dhruvmaurya066@gmail.com>
2026-09-16 22:53:45 +0530

    binary search

--- search/Search.java
+++ search/Search.java
@@ -10,14 +10,39 @@ public class Search {
         }
         return -1;
     }
+    static int BinarySearch(int[] arr,int key){
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(arr[mid] == key){
+                return mid;
+            }else if(arr[mid] < key){
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return -1;
+
+    }
 
     static void main() {
         int[] arr ={1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Linear Search ");
         int status= LinearSearch(arr,6);
         if(status==-1){
             System.out.println("Element not found");
         }else {
             System.out.println("Element found at index "+status+" in array");
+        }
+        System.out.println("Binary Search ");
+        int status1 = BinarySearch(arr,10);
+        if(status1==-1){
+            System.out.println("Element not found");
+        }else  {
+            System.out.println("Element found at index "+status1+" in array");
         }
 
     }
